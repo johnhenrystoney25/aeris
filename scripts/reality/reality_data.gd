20ab4d8b4e2ec9aebb467b854b72ade54aa21877
johnhenrystoney25 <johnhenrystoney@gmail.com>
2026-09-09 00:43:19 -0700

    Add data-driven reality resource

---
+++ scripts/reality/reality_data.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name RealityData
+
+@export var id := "main_city"
+@export var display_name := "Main City"
+@export var gravity := 1.0
+@export var time_scale := 1.0
+@export var size_multiplier := 1.0
+@export var destruction_level := 0.1
+@export var magic_level := 0.0
+@export var technology_level := 1.0
+@export var available_abilities: Array[StringName] = []
